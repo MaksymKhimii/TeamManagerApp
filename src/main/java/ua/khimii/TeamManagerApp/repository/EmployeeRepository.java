cb18717135294d30e15c633dd1924c4f334d2632
Maksym Khimii <meizovicmaks@gmail.com>
2023-02-03 23:03:32 +0200

    completed EmployeeService

--- src/main/java/ua/khimii/TeamManagerApp/repository/EmployeeRepository.java
+++ src/main/java/ua/khimii/TeamManagerApp/repository/EmployeeRepository.java
@@ -3,6 +3,11 @@ package ua.khimii.TeamManagerApp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ua.khimii.TeamManagerApp.model.Employee;
 
+import java.util.Optional;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
 }
--- src/main/java/ua/khimii/TeamManagerApp/service/EmployeeService.java
+++ src/main/java/ua/khimii/TeamManagerApp/service/EmployeeService.java
@@ -2,9 +2,11 @@ package ua.khimii.TeamManagerApp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ua.khimii.TeamManagerApp.exception.UserNotFoundException;
 import ua.khimii.TeamManagerApp.model.Employee;
 import ua.khimii.TeamManagerApp.repository.EmployeeRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -16,9 +18,24 @@ public class EmployeeService {
         this.employeeRepository = employeeRepository;
     }
 
-    public Employee addEmployee(Employee employee){
+    public Employee addEmployee(Employee employee) {
         employee.setEmployeeCode(UUID.randomUUID().toString());
         return employeeRepository.save(employee);
     }
 
+    public List<Employee> findAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id) throws Throwable {
+        return employeeRepository.findEmployeeById(id)
+                .orElseThrow(()-> new UserNotFoundException("User by id "+ id +" was not found"));
+    }
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteEmployeeById(id);
+    }
 }
